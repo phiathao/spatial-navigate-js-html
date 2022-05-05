75c5e9bf08be10df50bde1c0aed97edfd40deb8d
Phia <phiat11@gmail.com>
2022-05-05 15:02:30 -0500

    example init

--- spatial.js
+++ spatial.js
@@ -1,56 +1,42 @@
-
-type spatialOption = {
-    focusOption: "firstElement" | "lastElement"
-    mouseEnabled: boolean
-}
-
 // if no option is set, option will be set to default
 var defaultOption = {
     focusOption: "firstElement",
-    mouseEnabled: false,
+    mouseEnabled: false
 };
-
 var _option = {
     focusOption: defaultOption.focusOption,
     mouseEnabled: defaultOption.focusOption
-}
-
+};
 // store variable to handle movement
 var _lastElement = {};
 var _lastHoverElement = undefined;
-var _sections:HTMLElement = undefined;
+var _sections = undefined;
 var _mouseEnabled = false;
-
 // reset variable
 function cleanup() {
-    _lastElement = {}
+    _lastElement = {};
     _lastHoverElement = undefined;
     _sections = undefined;
     _mouseEnabled = false;
 }
-
 // hover custom event
 var hoverEvent = new CustomEvent("hover");
 var hoverLeaveEvent = new CustomEvent("hover-leave");
-
 window.addEventListener("hashchange", cleanup);
-
 var Spatial = {
-
     // start spatial navigation
-    init: (inputOption: spatialOption | undefined) => {
-        if ( inputOption && typeof inputOption === "object" ){
+    init: function (inputOption) {
+        if (inputOption && typeof inputOption === "object") {
             //define spatial option
-            Object.keys(inputOption).map((k, i) => {
+            Object.keys(inputOption).map(function (k, i) {
                 if (_option[k]) {
                     _option[k] = inputOption[k];
                 }
             });
         }
-
         // define selectable section base off html
-        let selectableElement = document.querySelector<HTMLElement>('[focusable-section="true"]');
-        _sections = selectableElement
+        var selectableElement = document.querySelector('[focusable-section="true"]');
+        _sections = selectableElement;
     }
-}
+};
 console.log("spatial script added");
